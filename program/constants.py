f2a1129b38405522b1f6d37d46338a39c3d5ace6
Deee <139545183+DAMI333N@users.noreply.github.com>
2024-01-13 19:29:50 +0800

    Stage 2 -Construct Market Prices - Preparation

--- program/constants.py
+++ program/constants.py
@@ -1,11 +1,11 @@
-from dydx3.constants import API_HOST_GOERLI, API_HOST_MAINNET
+from dydx3.constants import API_HOST_SEPOLIA, API_HOST_MAINNET
 from decouple import config
 
 # !!!! SELECT MODE !!!!
-MODE = "DEVELOPMENT"
+MODE = "DEVELOPMENT" # switch between PRODUCTION or DEVELOPMENT
 
 # Close all open positions and orders
-ABORT_ALL_POSITIONS = True
+ABORT_ALL_POSITIONS = False
 
 # Find Cointegrated Pairs
 FIND_COINTEGRATED = True
@@ -20,7 +20,7 @@ PLACE_TRADES = True
 RESOLUTION = "1HOUR"
 
 # Stats Window
-WINDOW = 21
+WINDOW = 21 # Rolling average based on 21 days
 
 # Thresholds - Opening
 MAX_HALF_LIFE = 24
@@ -32,7 +32,7 @@ USD_MIN_COLLATERAL = 1880
 CLOSE_AT_ZSCORE_CROSS = True
 
 # Ethereum Address
-ETHEREUM_ADDRESS = "0xe02809A6fEd4F98D6Ed8974C02818c9e3b7e7448"
+ETHEREUM_ADDRESS = "0xD423bFaD7E1241B249F36CFE3F33027Af182B191"
 
 # KEYS - PRODUCTION
 # Must be on Mainnet in DYDX
@@ -55,9 +55,9 @@ DYDX_API_SECRET = DYDX_API_SECRET_MAINNET if MODE == "PRODUCTION" else DYDX_API_
 DYDX_API_PASSPHRASE = DYDX_API_PASSPHRASE_MAINNET if MODE == "PRODUCTION" else DYDX_API_PASSPHRASE_TESTNET
 
 # HOST - Export
-HOST = API_HOST_MAINNET if MODE == "PRODUCTION" else API_HOST_GOERLI
+HOST = API_HOST_MAINNET if MODE == "PRODUCTION" else API_HOST_SEPOLIA
 
 # HTTP PROVIDER
-HTTP_PROVIDER_MAINNET = "https://eth-mainnet.g.alchemy.com/v2/C38A2E03uos12XB0zaw4OhkHEZm5Io8T"
-HTTP_PROVIDER_TESTNET = "https://eth-goerli.g.alchemy.com/v2/b9CmjVOibZ3s90EAMdjalLg8Q4sVzfdQ"
+HTTP_PROVIDER_MAINNET = "https://eth-mainnet.g.alchemy.com/v2/YbqpVIBezuXDyWAcvPyJP2ZFrybx5uen"
+HTTP_PROVIDER_TESTNET = "https://eth-sepolia.g.alchemy.com/v2/HIlalhe68kcq7jbcbduFdrU8cELAdV4X"
 HTTP_PROVIDER = HTTP_PROVIDER_MAINNET if MODE == "PRODUCTION" else HTTP_PROVIDER_TESTNET
